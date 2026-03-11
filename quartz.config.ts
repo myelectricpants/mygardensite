6b4e8296288b515412d198c1a10b8cf9f2635e22
myelectricpants <joshkimmel@gmail.com>
2026-03-11 09:33:44 -0400

    fixed fonts

--- quartz.config.ts
+++ quartz.config.ts
@@ -23,10 +23,10 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
-        code: "IBM Plex Mono",
-      },
+  header: "Inter",
+  body: "Lora",
+  code: "IBM Plex Mono",
+},
       colors: {
         lightMode: {
           light: "#faf8f8",
